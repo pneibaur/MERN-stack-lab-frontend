3e356905f7206a2e88f2dc71313150b798f54716
Phil Neibaur <pneibaur@Phils-MacBook-Pro.local>
2022-09-26 15:20:04 -0600

    begins frontend routes

---
+++ src/pages/Index.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Index = (props) => {
+    const loaded = () => {
+        return props.people.map((person) => (
+            <div key={person._id} className="person">
+                <Link to={`/people/${person._id}`}><h1>{person.name}</h1> </Link>
+                <img src={person.image} alt={person.name} />
+                <h3>{person.title}</h3>
+            </div>
+        ))
+    }
+    const loading = () => {
+        return <h1>Loading...</h1>
+    }
+    return (
+        props.people ? loaded() : loading()
+    )
+}
+
+export default Index
