e851b133ac550c373977a44494f720fbae71d869
Phil Neibaur <pneibaur@Phils-MacBook-Pro.local>
2022-10-01 18:56:41 -0600

    adds footer links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className='footer'>
-        <span>Phil Neibaur | <a href="https://github.com/pneibaur">GitHub</a> | <a href="https://www.linkedin.com/in/phillip-neibaur/">LinkedIn</a></span>
+        <span>Phil Neibaur | <a target="_blank" href="https://github.com/pneibaur">GitHub</a> | <a target="_blank" href="https://www.linkedin.com/in/phillip-neibaur/">LinkedIn</a></span>
     </footer>
   )
 }
